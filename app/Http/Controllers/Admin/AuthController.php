551f5a02365b73343954a4269789d150af0204c8
StanislavHrechan <network.stanislav@gmail.com>
2026-06-04 12:39:16 +0200

    Update admin dash

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -32,6 +32,6 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('auth.login');
+        return redirect()->route('home');
     }
 }
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,7 +28,7 @@
         <nav class="flex items-center gap-1 bg-white/5 backdrop-blur-md 
                     border border-white/10 rounded-full p-1 shadow-lg z-50">
 
-           <a href=""
+           <a href="{{route('admin.dashboard')}}"
             class="flex items-center gap-2 px-4 py-2 rounded-full text-sm
             {{ request()->routeIs('admin.dashboard') ? 'bg-white text-black' : 'text-white/70 hover:text-white hover:bg-white/10' }}">
 
